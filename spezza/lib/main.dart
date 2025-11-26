fc6c03db5a74f544fb918812abeb727810548705
KukoBerry <JSSacademica@proton.me>
2025-11-26 15:04:19 -0300

    FEAT: instalar supabase básico

--- spezza/lib/main.dart
+++ spezza/lib/main.dart
@@ -1,22 +1,36 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
 part 'main.g.dart';
 
-void main() {
-  runApp(ProviderScope(child: MaterialApp(home: MyApp())));
+void main() async {
+  await Supabase.initialize(
+    url: 'https://twztngpknaldevuxjpwi.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3enRuZ3BrbmFsZGV2dXhqcHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzUxNjcsImV4cCI6MjA3OTc1MTE2N30.vSpV5c9Xj2Ob0Me35c6arJHEXPzhoaYpJzthYSDY5IA',
+  );
+  final List<Map<String, dynamic>> results = await Supabase.instance.client.from('budgetgoals').select();
+
+  runApp(ProviderScope(child: MaterialApp(home: MyApp(results))));
 }
 
 class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+  final List<Map<String, dynamic>> results;
+  const MyApp(this.results, {super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     int numeroSorteado = ref.watch(sorteioProvider);
-    return Scaffold(body: Text('Número: $numeroSorteado'));
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(results.toString(), textDirection: TextDirection.ltr),
+          Text('Número: $numeroSorteado'),
+        ],
+      )
+    );
   }
 }
 
--- spezza/test/widget_test.dart
+++ spezza/test/widget_test.dart
@@ -14,6 +14,7 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
+    //ñ sei se a linha de cima precisa ser corrigida ou se deletar esse arquivo ñ tem problema
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
