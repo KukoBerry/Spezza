cf084127f48d15b19f531d9555ba9ea6785bd628
fabriciosilvas <fabriciosilva@ufba.br>
2025-12-16 18:06:12 -0300

    Add colors

---
+++ spezza/lib/shared/utils/graphic_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colors = [
+  Color(0xFF008000),
+  Color(0xFF83814C),
+  Color(0xFF87AB7B),
+  Color(0xFF8DF075),
+  Color(0xFFA3AF9E),
+  Color(0xFF809E1B),
+  Color(0xFF5E7A51),
+  Color(0xFF008E7B),
+  Color(0xFF9E6200),
+  Color(0xFFF0DEB4),
+];
